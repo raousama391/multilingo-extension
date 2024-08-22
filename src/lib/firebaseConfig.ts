f8506c42295b5576d4b6c59d90efd6a5f30aeabe
Muhammad Ahsan Farooq <ahsanfarooq210@gmail.com>
2024-08-22 20:43:23 +0500

    added firebase to the project

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWSvc-S8h9TuR28WKp8or6o_KQUEwERgI",
+  authDomain: "multilingo-extention.firebaseapp.com",
+  projectId: "multilingo-extention",
+  storageBucket: "multilingo-extention.appspot.com",
+  messagingSenderId: "925790065182",
+  appId: "1:925790065182:web:881fdf09da21728776940f",
+  measurementId: "G-6MZ0MV4Y7V",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
